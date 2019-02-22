415351be9107e76693c42476d9e5b7edb7dd1c1f
Vytautas <vytautas@whatagraph.com>
2019-02-22 22:43:35 +0200

    Using vectors now

--- sketch.js
+++ sketch.js
@@ -5,13 +5,12 @@ function setup() {
   createCanvas(800, 800);
 
   preys.push(new Prey(round(random(width) / 5) * 5, round(random(height) / 5) * 5))
-
   predators.push(new Predator(round(random(width) / 5) * 5, round(random(height) / 5) * 5))
 }
 
 function draw() {
   frameRate(144)
-  background(255);
+  background('rgba(255,255,255, 0.5)');
 
   for (let i = 0; i < preys.length; i++) {
     preys[i].draw()
@@ -23,7 +22,7 @@ function draw() {
   if (spawn >= 0.7 && preys.length !== 0) {
     let parent = floor(random(preys.length))
 
-    preys.push(new Prey(preys[parent].x, preys[parent].y))
+    preys.push(new Prey(preys[parent].position.x, preys[parent].y))
   }
 
   let predatorHasEaten = -1
@@ -34,34 +33,13 @@ function draw() {
     
     end_loops:
     for (let k = 0; k < preys.length; k++) {
-      for (let row = -1; row < 2; row++) {
-        for (let col = -1; col < 2; col++) {
-          if (preys[k].x === predators[i].x + (row * 5) && preys[k].y === predators[i].y + (col * 5)) {
-            predatorHasEaten = i
-            preys.splice(k, 1)
-            break end_loops
-          }
-        }
+      if (dist(predators[i].position.x, predators[i].position.y, preys[k].position.x, preys[k].position.y) <= 10) {
+      // if (preys[k].position.x === predators[i].position.x + (row * 5) && preys[k].position.y === predators[i].position.y + (col * 5)) {
+        predatorHasEaten = i
+        preys.splice(k, 1)
+        break end_loops
       }
     }
-
-//     let preyIndex = preys.findIndex(function(prey) {
-
-//       for (let row = -1; row < 2; row++) {
-//         for (let col = -1; col < 2; col++) {
-//           if (prey.x == predators[i].x + row && prey.y == predators[i].y + col) {
-//             predatorHasEaten = i
-//             return true
-//           }
-//         }
-//       }
-
-//       return false
-//     })
-
-    // if (preyIndex !== -1) {
-    //   preys.splice(preyIndex, 1)
-    // }
   }
 
   if (predatorHasEaten !== -1) {
@@ -69,7 +47,7 @@ function draw() {
     let predatorShouldSpawn = random()
 
     if (predatorShouldSpawn >= 0.5) {
-      predators.push(new Predator(predators[predatorHasEaten].x, predators[predatorHasEaten].y))
+      predators.push(new Predator(predators[predatorHasEaten].position.x, predators[predatorHasEaten].position.y))
     }
   }
   
@@ -79,6 +57,7 @@ function draw() {
     predators.splice(floor(random(predators.length)), 1)
   }
 
+  noStroke()
   fill(0, 0, 255)
   text(preys.length, 20, 20)
   fill(255, 0, 0)
@@ -88,57 +67,41 @@ function draw() {
 
 class Prey {
   constructor(x, y) {
-    this.x = x
-    this.y = y
+    this.position = createVector(x, y)
+    this.velocity = p5.Vector.random2D()
+    this.speed = 2
   }
 
   draw() {
-    fill(0, 0, 255)
-    rect(this.x, this.y, 5, 5)
+    // fill(0, 0, 255)
+    strokeWeight(5)
+    stroke(0, 0, 255)
+    point(this.position.x, this.position.y)
+    // rect(this.x, this.y, 5, 5)
   }
 
   move() {
-    let direction = round(random(7))
 
-    switch (direction) {
-      case 0:
-        this.x -= 5;
-        this.y -= 5;
-        break;
-      case 1:
-        this.y -= 5;
-        break;
-      case 2:
-        this.x += 5;
-        this.y -= 5;
-        break;
-      case 3:
-        this.x -= 5;
-        break;
-      case 4:
-        this.x += 5;
-        break;
-      case 5:
-        this.x -= 5;
-        this.y += 5;
-        break;
-      case 6:
-        this.y += 5;
-        break;
-      case 7:
-        this.x += 5;
-        this.y += 5;
-        break;
-    }
+    this.velocity.add(p5.Vector.random2D())
+    this.velocity.setMag(this.speed)
+    this.velocity.limit(5)
+    this.position.add(this.velocity)
 
-    this.x = (this.x + width) % width
-    this.y = (this.y + height) % height
+    this.position.x = (this.position.x + width) % width
+    this.position.y = (this.position.y + height) % height
   }
 }
 
 class Predator extends Prey {
+  constructor(x, y) {
+    super(x, y)
+    this.speed = 3
+  }
+
   draw() {
-    fill(255, 0, 0)
-    rect(this.x, this.y, 5, 5)
+    stroke(255, 0, 0)
+    point(this.position.x, this.position.y)
+    // fill(255, 0, 0)
+    // rect(this.x, this.y, 5, 5)
   }
 }
